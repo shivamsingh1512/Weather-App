d1c128d35ee4d4e6c2eb0c3c1f62050419a96750
shivamsingh1512 <bwubta21169@brainwareuniversity.ac.in>
2024-05-19 23:15:33 +0530

    created index.ejs

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 
 const port = 3000;
 
+app.set("views engine" , "ejs");
+//app.set("views" , path.join(__dirname , "/views"));
+
+app.get("/" , (req,res) => {
+    res.render("index.ejs");
+});
+
 app.use("/static",express.static ("public"));
 
 app.listen (port, () =>{
