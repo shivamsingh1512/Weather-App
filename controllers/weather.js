8efb26d646edfde0cd8d2cbfa6b32c6838f37939
shivamsingh1512 <bwubta21169@brainwareuniversity.ac.in>
2024-05-26 17:59:20 +0530

    created a weather api

--- controllers/weather.js
+++ controllers/weather.js
@@ -4,7 +4,17 @@ import cookieSession from "cookie-session";
 import bodyParser from "body-parser";
 import { findUserIdByName } from "../models/users.js";
 import store from "store";
+import dotenv from "dotenv";
+dotenv.config();
 
+async function getWeather(location) {
+    let apikey = process.env.API_KEY;
+    const d = new Date();
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apikey}`);
+    const weatherResult = await response.json();
+    console.log(weatherResult);
+    return weatherResult;
+  }
 
 export async function weatherReport(req,res){
     // let username = req.session.username;
@@ -16,6 +26,8 @@ export async function postWeatherReport(req,res){
     let username = store.get("username");
     let password = store.get("password");
     let user = await findUserIdByName(username, password);
-    res.send(user);
+    let cityname = req.body.cityname;
+    let wr = getWeather(cityname);
+    res.json(wr);
 }
 
